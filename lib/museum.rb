ad89b8793b8423207e6bb4db48f72b18a67f6d79
Austin Aspaas <araspaas@live.com>
2020-05-27 10:54:47 -0600

    Got stuck on last method in iteration 2. was doing good til then. very frustrated in myself.

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,4 +10,18 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    add_exhibit
+    patron = patron.name
+    @exhibits.each do |exhibit|
+      exhibit.each do |patron|
+        if patron.interests == @exhibits[1] && @exhibits[0]
+          recommend_exhibits(patron)
+        else patron.interests == @exhibits[2]
+          recommend_exhibits(patron)
+        end
+      end
+    end
+  end
 end
